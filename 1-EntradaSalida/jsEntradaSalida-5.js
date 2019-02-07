4b637bc85d05241f310d1df816e8128e85157e95
alumno <alumno>
2019-02-06 21:25:20 -0300

    ejercicio 7 realizado

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,15 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var nombre;
+	var edad;
+
+	edad = laEdad.value;
+	nombre = elNombre.value;
+
+	alert (nombre+edad);
+	alert("su nombre es " +nombre+ " y su edad es  "+edad+" años.");
+
+
 }
 
