9ae023a50f91d42e5560a62efc63676334837114
omar <you@example.com>
2019-02-14 17:13:44 -0300

    hice ejercicio 9 del if

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+    var laedad;
+
+    laedad=edad.value;
+    laedad = parseInt(laedad);
+    estado = estadoCivil.value;
+    if ( laedad<18 ) {
+
+    }
+        else{
+            if (estado== "Soltero") {
+                    alert("usted es soltero y  no es menor");
+                }
+            }
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
